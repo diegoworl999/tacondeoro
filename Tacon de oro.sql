f447c21d4927aff3fbd6fc75cad21245b70ed913
Alejandro-Garcia-Morato <126868044+Alejandro-Garcia-Morato@users.noreply.github.com>
2023-04-19 11:06:12 +0200

    V2.0
    Corrección de tablas

--- Tacon de oro.sql
+++ Tacon de oro.sql
@@ -2,18 +2,10 @@ Drop database if exists tacondeoro;
 create database tacondeoro;
 use tacondeoro;
 
-create table Tarjetabancaria
-(numero varchar(50),
-primary key (numero));
-
-create table ruta
-(areas varchar (100),
-diareparto varchar(50),
-primary key (areas)
-);
-
+drop table if exists socio;
 create table socio
-(nombre varchar(50),
+(id int not null unique,
+nombre varchar(50),
 correoe varchar(100),
 direccion varchar(50),
 poblacion varchar(100),
@@ -22,40 +14,64 @@ primary key (nombre),
 foreign key (numerotar) references Tarjetabancaria (numero),
 foreign key (direccion) references ruta (areas));
 
+drop table if exists tarjetabancaria;
+create table tarjetabancaria
+(numero varchar(50) not null unique,
+idSocio int,
+primary key (numero)
+foreign key (idSocio) references socio (id));
+
+drop table if exists ruta;
+create table ruta
+(areas varchar (100) not null unique,
+diareparto varchar(50),
+primary key (areas)
+);
+
+drop table if exists pedidos;
 create table pedidos
-(codigo int,
+(codigo int not null unique,
 fecha varchar(50),
 total decimal,
-nomsocio varchar(50),
+idsocio int,
 primary key (codigo),
-foreign key(nomsocio) references socio (nombre));
+foreign key(idsocio) references socio (id));
 
+drop table if exists empresatransporte;
 create table empresatransporte
-(nombre varchar(100),
+(nombre varchar(100) not null unique,
 CIF varchar(100),
 domicilio varchar(100),
 primary key (nombre),
 foreign key (domicilio) references ruta (areas)
 );
 
-create table articulos
-(id int,
+drop table if exists articulos;
+create table articulo
+(id int not null unique,
 nombre varchar (50),
 tipoarticulo varchar (50),
 precio decimal,
+color varchar(50),
 descripcion varchar (500),
 material varchar (50), 
 stock int,
 foto varchar (200),
+numeroZapato int,
+tipoZapato varchar(50),
+tipoBolso varchar(50),
+tipoComlemento varchar(50),
+tallaComplemento int,
 primary key (id)
 );
 
+drop table if exists lineaspedido;
 create table lineaspedido
-(codpe int,
-ida int,
+(codPedido int not null,
+idArticulo int not null,
 cantidad int,
 primary key (codpe, ida),
 unique (codpe, ida),
-foreign key (codpe) references pedidos (codigo),
-foreign key (ida) references articulos (id) 
+foreign key (codPedido) references pedidos (codigo),
+foreign key (idArticulo) references articulos (id) 
 );
